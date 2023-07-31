1265a4d85cf6cb1c57fc078a5db9fe41bb7de7fb
Michael Ruppert <michael.ruppert@fau.de>
2023-07-31 02:20:30 +0200

    ...

--- studie.py
+++ studie.py
@@ -1,95 +1,15 @@
 import random
 from org.transcrypt.stubs.browser import document
-import math
 
-
-class math:
-    pi = 3.14159265359
-    e = 2.718281828459045
-
-    @staticmethod
-    def ln(x, n_terms=100):
-        return sum([((-1) ** (n - 1)) * (x - 1) ** n / n for n in range(1, n_terms + 1)])
-
-    @staticmethod
-    def ln_gamma(z, n_terms=100):
-        g = [7.1562499999993797, 0.1659470187408462e-06,
-             0.1659470187408462e-06, 0.1659470187408462e-06,
-             0.1659470187408462e-06, 0.16060597132059125e-01,
-             0.16060597132059125e-01, 0.53320781342957658,
-             0.53320781342957658, 0.53320781342957658,
-             0.20580808427784533e+01, 0.14065092762033e+03,
-             0.14065092762033e+03, 0.49094141100024683e+05,
-             0.4930417745369179e+07, 0.4922931438387461e+09,
-             0.999165130434776587, 0.99999999999980993, 1.000000000000000]
-
-        x = 0.99999999999980993
-        for i in range(1, 18):
-            x += g[i] / (z + i)
-        c = 2 * math.pi / (x * g[0])
-        log_c = math.ln(c, n_terms)
-        return log_c + (z + 0.5) * math.ln(z + 5.5, n_terms) - (z + 5.5) + math.ln(x * g[0], n_terms)
-
-
-    @staticmethod
-    def ln_factorial(n):
-        return sum(math.ln(i) for i in range(1, n + 1))
-
-    @staticmethod
-    def exp(x, n_terms=100):
-        return sum((x ** n) / math.ln_factorial(n) for n in range(n_terms))
-
-    @staticmethod
-    def gamma(z, n_terms=100):
-        return math.exp(math.ln_gamma(z, n_terms), n_terms)
-
-    @staticmethod
-    def sqrt(x):
-        return x ** 0.5
-
-    @staticmethod
-    def beta(x, y):
-        return math.gamma(x) * math.gamma(y) / math.gamma(x + y)
-
-
-
-
-def beta(x, y):
-    # Die Beta-Funktion basierend auf der Gamma-Funktion
-    return math.gamma(x) * math.gamma(y) / math.gamma(x+y)
-
-def t_distribution(x, df):
-    # Die Dichtefunktion der T-Verteilung
-    ln_result = (math.gamma((df+1)/2) - math.ln(math.sqrt(df*math.pi))
-                 - math.gamma(df/2) - ((df+1)/2) * math.ln(1 + (x**2/df)))
-    print(ln_result)
-    return math.exp(ln_result)
-
-def t_cdf(t, df):
-    # Die kumulative Dichtefunktion der T-Verteilung
-    # Anwendung der Simpson-Regel zur numerischen Integration
-    x = abs(t)
-    a = 0
-    b = x
-    n = 1000  # Anzahl der Teilintervalle
-    h = (b-a) / n
-    print(h)
-    integral = t_distribution(a, df) + t_distribution(b, df) + 4*t_distribution(b-h, df)
-    print(integral)
-    for i in range(1, n//2):
-        integral += 4*t_distribution(a + 2*i*h, df) + 2*t_distribution(a + (2*i-1)*h, df)
-        print(integral)
-    integral *= h / 3
-    return 0.5 + (integral / math.sqrt(df*math.pi/beta(0.5, df/2)))
 
 def t_to_p(t, df):
-    print(t,df)
+    print(t, df)
     # Umwandeln eines T-Wertes in einen P-Wert
-    cdf = t_cdf(t, df)
-    if t > 0:
-        return 1-cdf
-    else:
-        return cdf
+    pValue = window.calculate_p_value(t, df)
+    return pValue
+
+
+
 
 class PainEvents:
     def __init__(self):
